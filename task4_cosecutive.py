29a68ebdfd0e44a281b1c5d77e54ec0d9875c0da
Asp2591 <patkaratharva75@gmail.com>
2025-06-29 23:01:25 +0530

    task4 updated

--- task4_cosecutive.py
+++ task4_cosecutive.py
@@ -16,17 +16,17 @@ nums = [0,1,2,3,5,6,7]
 def longest_consecutive(nums: list) -> int:
     if not nums:
         return 0
-    nums.sort()
-    longest = 1
-    streak = 1
+    nums.sort()     #sorted the list
+    maximum_streak = 1         #longest consecutive streak
+    current_streak = 1         #current longest streak
     for i in range(1, len(nums)):
-        if nums[i] == nums[i-1]:
+        if nums[i] == nums[i-1]:  #finding duplicate numbers
             continue
-        elif nums[i] == nums[i-1] + 1:
-            streak += 1
+        elif nums[i] == nums[i-1] + 1:  #finding ith index and (i+1)th index are consecutive
+            current_streak += 1
         else:
-            longest = max(longest, streak)
-            streak = 1
-    return max(longest, streak)
+            maximum_streak = max(maximum_streak, current_streak)  #whichever maximum or current streak would be maximum streak
+            current_streak = 1      #restart the streak
+    return max(maximum_streak, current_streak)  
 
 print(longest_consecutive(nums))
