180b834443667918c20180614f36a8cce53ab9a8
Asp2591 <patkaratharva75@gmail.com>
2025-07-03 20:57:48 +0530

    task 4 logic changed

--- Python Tasks round 2/Task2_4_Decimal_to_Roman.py
+++ Python Tasks round 2/Task2_4_Decimal_to_Roman.py
@@ -8,17 +8,17 @@ Using library roman to convert by importing it
 
 '''
 
-import roman
-num=3999
-def decimal_to_roman(num):
-    if num>=1 and num<=3999:
-        num=roman.toRoman(num)
-    else:
-        return 'Number is not between 1 to 3999'
-    return num
+# import roman
+# num=3999
+# def decimal_to_roman(num):
+#     if num>=1 and num<=3999:
+#         num=roman.toRoman(num)
+#     else:
+#         return 'Number is not between 1 to 3999'
+#     return num
 
 
-print(decimal_to_roman(num))
+# print(decimal_to_roman(num))
 
 
 
@@ -28,28 +28,35 @@ print(decimal_to_roman(num))
 '''
 Using list of fixed roman values of decimal numbers.
 '''
-num=7600
-
+num=1987
 def decimal_to_roman(num):
-    if num>=1 and num<=3999:
+    if num >= 1 and num <= 3999:
+        roman_val_symb = {
+            1000: 'M',
+            900: 'CM',
+            500: 'D',
+            400: 'CD',
+            100: 'C',
+            90: 'XC',
+            50: 'L',
+            40: 'XL',
+            10: 'X',
+            9: 'IX',
+            5: 'V',
+            4: 'IV',
+            1: 'I'
+        }
 
-        val=[
-        1000,900,500,400,100,90,50,40,10,9,5,4,1
-    ]
-        romanSymb=[
-        'M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'
-    ]
-        romanStr=''
-        i=0
-        while num > 0:
-            count=num//val[i]                #1=6//5
-            romanStr+=romanSymb[i]*count     #'MCDLV'='MCDLVI'*1
-            num-=val[i]*count                  # 1456=6-(5*1)=1
-            i+=1                            #i=10
-    
+        romanStr = ''
+        for val in roman_val_symb:
+            count = num // val
+            romanStr += roman_val_symb[val] * count
+            num -= val * count
+
         return romanStr
     else:
         return "Number is not between 1 to 3999"
+
 
 print(decimal_to_roman(num))
 
