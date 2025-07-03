2527e83d8bece3754386161ba5e220c67f26d108
Asp2591 <patkaratharva75@gmail.com>
2025-07-03 20:26:41 +0530

    task 4 logic changed

--- Python Tasks round 2/Task2_4_Decimal_to_Roman.py
+++ Python Tasks round 2/Task2_4_Decimal_to_Roman.py
@@ -9,9 +9,12 @@ Using library roman to convert by importing it
 '''
 
 import roman
-num=1956
+num=3999
 def decimal_to_roman(num):
-    num=roman.toRoman(num)
+    if num>=1 and num<=3999:
+        num=roman.toRoman(num)
+    else:
+        return 'Number is not between 1 to 3999'
     return num
 
 
@@ -25,24 +28,28 @@ print(decimal_to_roman(num))
 '''
 Using list of fixed roman values of decimal numbers.
 '''
-num=1956
+num=7600
 
 def decimal_to_roman(num):
-    val=[
+    if num>=1 and num<=3999:
+
+        val=[
         1000,900,500,400,100,90,50,40,10,9,5,4,1
     ]
-    romanSymb=[
+        romanSymb=[
         'M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'
     ]
-    romanStr=''
-    i=0
-    while num > 0:
-        count=num//val[i]                #1=6//5
-        romanStr+=romanSymb[i]*count     #'MCDLV'='MCDLVI'*1
-        num-=val[i]*count                  # 1456=6-(5*1)=1
-        i+=1                            #i=10
-
-    return romanStr
+        romanStr=''
+        i=0
+        while num > 0:
+            count=num//val[i]                #1=6//5
+            romanStr+=romanSymb[i]*count     #'MCDLV'='MCDLVI'*1
+            num-=val[i]*count                  # 1456=6-(5*1)=1
+            i+=1                            #i=10
+    
+        return romanStr
+    else:
+        return "Number is not between 1 to 3999"
 
 print(decimal_to_roman(num))
 
